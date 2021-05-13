a522649db52c043c72a6f57bc39cffacb8ee323c
charlierdm <charlie.davismiller@gmail.com>
2021-05-13 17:45:54 +0100

    landPlane and takOff functionality added - tests passing

--- airport.js
+++ airport.js
@@ -4,6 +4,17 @@ class Airport {
     this.capacity = capacity;
   }
 
+  landPlane(plane){
+    this.hanger.push(plane);
+    return 'The plane has been landed safely.';
+  }
+
+  takeOff(){
+    this.hanger.pop();
+    return `The plane has taken off succesfully, there are ${this.hanger.length} planes in the airport.`;
+  }
+
 }
 
 module.exports = Airport;
+
--- airport.test.js
+++ airport.test.js
@@ -1,9 +1,20 @@
 const Airport = require('./airport')
+const Plane = require('./plane')
 
 const airport = new Airport();
 
 test('the airport default capacity can be overridden as appropriate', () => {
-  let newAirport = new Airport(capacity = 5)
+  let newAirport = new Airport(capacity = 5);
   expect(newAirport.capacity).toBe(5);
 });
+
+test('the air traffic controller can instruct a plane to land at the airport', () => {
+  let plane = new Plane();
+  airport.landPlane(plane);
+  expect(airport.hanger.length).toEqual(1);
+})
+
+test('the air traffic controller can instruct a plane to take off and recieve confirmation', () => {
+  expect(airport.takeOff()).toEqual(`The plane has taken off succesfully, there are 0 planes in the airport.`);
+})
 
