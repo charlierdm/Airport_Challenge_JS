aec3f37547a5510fda04275d85f8825d9ef4e8fe
charlierdm <charlie.davismiller@gmail.com>
2021-05-19 17:58:01 +0100

    all user stories met - all tests passing

--- airport.test.js
+++ airport.test.js
@@ -3,8 +3,8 @@ const Airport = require('./airport')
 jest.mock('./plane');
 const Plane = require('./plane');
 
-const stormyWeather = {forecast : () => 'stormy'}
-const sunnyWeather = {forecast : () => 'sunny'}
+const stormyWeather = {forecast : () => 'stormy'};
+const sunnyWeather = {forecast : () => 'sunny'};
 
 describe('Airport', () => {
 
@@ -14,21 +14,21 @@ describe('Airport', () => {
   });
 
   it('allows the air traffic controller to instruct a plane to land at the airport', () => {
-    let airport = new Airport(1, sunnyWeather)
+    let airport = new Airport(1, sunnyWeather);
     airport.landPlane(Plane);
     expect(airport.hanger).toContain(Plane);
   })
 
   it('allows the air traffic controller to instruct a plane to take off and recieve confirmation', () => {
-    let airport = new Airport(1, sunnyWeather)
+    let airport = new Airport(1, sunnyWeather);
     airport.landPlane(Plane);
     expect(airport.takeOff()).toEqual(`The plane has taken off succesfully, there are 0 planes in the airport.`);
   })
 
   it('does not accept planes landing if at maximum capacity', () => {
-    let airport = new Airport(5, sunnyWeather)
+    let airport = new Airport(5, sunnyWeather);
     for(let i = 0; i < airport.capacity; i++){
-      airport.landPlane(Plane)
+      airport.landPlane(Plane);
     }
     expect(airport.hanger.length).toEqual(5)
   })
@@ -36,13 +36,13 @@ describe('Airport', () => {
   describe('Stormy weather conditions', () => {
 
     it('prevents takeoff when the weather is stormy', () => {
-      let airport = new Airport(1, stormyWeather)
-      airport.landPlane(Plane)
+      let airport = new Airport(1, stormyWeather);
+      airport.landPlane(Plane);
       expect(airport.takeOff()).toEqual('The plane cannot leave the airport in poor weather conditions.')
     })
   
     test('prevents landing when the weather is stormy', () => {
-      let airport = new Airport(1, stormyWeather)
+      let airport = new Airport(1, stormyWeather);
       expect(airport.landPlane(Plane)).toEqual('The plane cannot land in poor weather conditions.')
     })
   })
